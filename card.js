2e13c72f16069bd956a56eee687971125ca84fb4
Pangu <Pangu@DESKTOP-IF6DCCV>
2018-05-12 20:06:43 -0700

    finished functionality of two player with reset upped damange on attack and bahamut

--- card.js
+++ card.js
@@ -2,33 +2,60 @@ var cardName = {
     attack: {name: 'attack',
         address:'images/Cards/attackCard.jpg',
         effect: 1,
-        storedCard: ''},
+        ability: function () {
+            if ( targetPlayer.cardHeld == 'shield'){
+                //playshield noise
+            }else{
+                targetPlayer.health -= currentPlayer.currentAttackPower;
+                //play hurt noise
+            }
+        }},
     shield: {name: 'shield',
         address:'images/Cards/shieldCard.jpg' ,
         effect: 0,
-        storedCard: ''},
+        ability: function () {
+            currentPlayer.cardHeld = 'shield';
+            playerHeldCardEquipped( this.address );
+            //play shield equip noise.
+        }},
     heal: {name: 'heal',
         address:'images/Cards/healCard.jpg',
         effect: 1,//how much it heals
-        storedCard: ''},
+        ability: function () {
+            currentPlayer.health += this.effect;
+        }},
     bahamut: {name: 'bahamut',
         address:'images/Cards/bahamutCard.jpg',
-        effect: 1,
+        effect: 3,
         ability: function(){
             //damage both players.
             for (var i = 0; i < 2; i++){
                 totalPlayers[i].health -= this.effect;
             }
             setTimeout(resetDeck, 2000);
+            resetPlayerAbilitys();
         }},
     doubleStrike: {name: 'doubleStrike',
         address: 'images/Cards/doubleStrikeCard.jpg',
         effect: 2,
-        storedCard: ''},
+        ability: function () {
+            currentPlayer.cardHeld = 'doubleStrike';
+            currentPlayer.currentAttackPower *= this.effect;
+            playerHeldCardEquipped( this.address );
+
+            //play sound of strength
+        }},
     tripleStrike: {name: 'tripleStrike',
         address: 'images/Cards/tripleStrikeCard.jpg',
         effect: 3,
-        storedCard: ''},
+        ability: function () {
+            currentPlayer.cardHeld = 'tripleStrike';
+            currentPlayer.currentAttackPower *= this.effect;
+            playerHeldCardEquipped( this.address );
+
+            //play sound of war.
+            //replace the card on field.
+        }},
 };
 
 function Player( _name , _playerPosString) {
@@ -36,35 +63,32 @@ function Player( _name , _playerPosString) {
     this.playerPos = _playerPosString;
     this.health = 10;
     this.cardHeld = null; //holds name of card
-    this.cardAddress = null; //holds address of card
-    this.currentAttackPower = 1;
+    this.cardAddress = null; //holds address of card // remove this?
+    this.currentAttackPower = 2;
 
     this.updateStatDisplay = function(){
-        var healthSelector = _playerPosString+' .playerStat h3';
+        var healthSelector = this.playerPos + ' .playerStat h3';
         $(healthSelector).text(this.health);
+        if(this.health <= 0){
+            playerDeath = true;
+        }
     }
-    this.updatePlayerStats = function () {
-        //check if cardAbility has anything.
-        this.cardAbility(this.cardHeld);
-        //if so affect health or currentAttackPower
-    }
-    this.resetCard_Atk = function () {
+    this.resetPlayerStats = function () {
         this.cardHeld = null;
         this.cardAddress = null;
-        this.currentAttackPower = 1;
+        this.currentAttackPower = 2;
     }
-    this.attackPerson = function(target){ //attack other person.
-
-        //check if person has a shield.
-        //if not:
-            //target.health -= 1;
-        //if does:
-            //return and do nothing.
+    this.resetPlayerHealth = function (){
+        this.health = 10;
     }
-
 }
 
-Player.prototype.cardAbility = function (_cardName) {
-    //this will check the card in a switch statment
-    console.log(_cardName)
+function playerHeldCardEquipped( _imgAddress ){
+    //if card has revealed.
+    //then unreveal wait and reveal again.
+
+    var cardSlotElement = currentPlayer.playerPos + ' .card > .back';
+    $(cardSlotElement).attr('src', _imgAddress);
+    var cardSelector = currentPlayer.playerPos + ' .card'
+    $(cardSelector).addClass('revealed');
 }
--- main.js
+++ main.js
@@ -4,18 +4,16 @@ $().ready(initiateGame);
 var first_card_clicked =null;
 var second_card_clicked = null;
 var cardOrder = ['attack','attack','shield','bahamut','doubleStrike','tripleStrike','heal','doubleStrike','tripleStrike'];
-//old.
-var total_possible_matches = cardOrder.length/2;//number of total possible matches (in this case 2)
-var match_counter = 0;
-var attempt_counter = 0;
-var games_played = 0;
 
-var currentPlayer = true; //true is player 1
+var currentPlayerTurn = true; //true is player 1
 var screenClickable = false; // player notificatin will turn this into true. for player 1.
 
 var player1 = new Player('Player 1', '#player1');
 var player2 = new Player('Player 2', '#player2');
 var totalPlayers = [player1,player2];
+var targetPlayer;
+var currentPlayer;
+var playerDeath = false;
 
 function initiateGame() {
     addEventHandler();
@@ -25,7 +23,7 @@ function initiateGame() {
 
 function addEventHandler() {
     $('.game-area').on('click', '.card', flipCardToFront);
-    $('.reset').on('click', resetGame);
+    $('#reset').on('click', resetGame);
 }
 
 function randomizeCardOrderArr(){
@@ -58,80 +56,46 @@ function flipCardToFront() {
 
 
     var currentCard = $(this);
+    cardType = checkCardName($(currentCard).find('img').attr('src'));
+
     if (first_card_clicked === null && currentCard.hasClass('revealed')=== false){
         currentCard.addClass('revealed'); // reveal the current card by adding class.
         // here we check card ability
-        cardType = checkCardName($(currentCard).find('img').attr('src'));
         if (cardType != 'attack') {
             screenClickable = !screenClickable;
-            if(cardType === 'bahamut'){
-                console.log('ITs BAHAMUT!');
-                setTimeout(resetDeck, 2000); // this sets a reset for entire deck.
-                //do damange to both players
-            }
-            //if its bahamut reset deck., if it can be stored it will be.
+            activateCardAbility(cardType);
             setTimeout(displayCard, 1000, $(currentCard).find('img').attr('src')); //this will display a card.
-
             return
         }
-            //else if attack move on.
         first_card_clicked = currentCard;
 
     }else if (currentCard.hasClass('revealed')=== false){
         screenClickable = !screenClickable; // turn off clickablity
         second_card_clicked = currentCard;
         currentCard.addClass('revealed'); // reveal the current card by adding class.
-        attempt_counter++; //increments attempts since this is the second card.
 
         if ($(first_card_clicked).find('img').attr('src') === $(second_card_clicked).find('img').attr('src')){
-            match_counter++;
-
+            //really a check to see if attack matches.
+            activateCardAbility(cardType);
+            resetPlayerAbilitys(); //round is over reset abilities.
             setTimeout(displayCard, 1000, $(first_card_clicked).find('img').attr('src'));// ('#cardModal');
             //this is for attack only
             //do attack damage. to target player.
             setTimeout(resetDeck, 2000); // at 2000ms is when the player notification will be called. when that happens reset deck.
 
         }else{
-            //flip both cards back.
-            setTimeout(flipCardToBack, 1000, first_card_clicked, second_card_clicked);
-            //check for bahamut
             cardType = checkCardName($(currentCard).find('img').attr('src'));
             if (cardType === 'bahamut') {
-                console.log('ITS BAHAMUT');
-                //reset deck.
-                setTimeout(resetDeck, 2000);
+                activateCardAbility(cardType);
+                setTimeout(displayCard, 1000, $(currentCard).find('img').attr('src')); //this will display a card.
+            }else{
+                setTimeout(flipCardToBack, 1000, first_card_clicked, second_card_clicked);
             }
-
         }
     }else{
         console.log('you clicked this already!');
     }
-    updatePlayerStats();
-}
 
-function updatePlayerStats(){
-    for (var i = 0; i < 2; i++){
-        totalPlayers[i].updateStatDisplay();
-    }
-}
-
-function display_stats(){
-    $('.matches .value').text(match_counter);
-
-    var accuracyPercent = match_counter/attempt_counter;
-    accuracyPercent = accuracyPercent.toFixed(2);
-    if (isNaN(accuracyPercent)){
-        accuracyPercent = 0;
-    }
-
-    $('.accuracy .value').text(accuracyPercent + '%');
-    $('.attempts .value').text(attempt_counter);
-}
-
-function reset_stats(){
-    $('.accuracy .value').text('0%');
-    $('.attempts .value').text('0');
-    $('.matches .value').text('0');
 }
 
 function flipCardToBack(jQueryElement1,jQueryElement2) {
@@ -140,20 +104,14 @@ function flipCardToBack(jQueryElement1,jQueryElement2) {
     resetFirstandSecondCardVar();
 }
 
-function checkWinCondition() {
-    if (match_counter===total_possible_matches){
-        console.log('WINNER WINNER CHICKEN DINNER');
-    }
-}
-
 function resetGame() {
-    //reset_stats();
-    //display_stats();
-    games_played++;
-    //$('.games-played .value').text(games_played);
-
-    //reset game.
-    resetDeck()
+    resetDeck();
+    player1.resetPlayerHealth();
+    player2.resetPlayerHealth();
+    resetPlayerAbilitys();
+    playerDeath = false;
+    resetFirstandSecondCardVar();
+    updatePlayerStats();
     
 }
 
@@ -175,7 +133,7 @@ function flipAllCardsOver(){
 function resetFirstandSecondCardVar() {
     first_card_clicked = null;
     second_card_clicked = null;
-    currentPlayer = !currentPlayer;
+    currentPlayerTurn = !currentPlayerTurn;
     currentPlayerPositionNotification(); // this will notify who should pick next.
 }
 
@@ -183,20 +141,61 @@ function displayCard(_inputCardSrc){
     $('#imgCardModal').attr('src', _inputCardSrc);
     toggleModal('#cardModal')
     setTimeout(toggleModal, 1500,'#cardModal'); //un toggle modal
+
+    updatePlayerStats(); // this updates player stats at the end of each attack or display
+    if(playerDeath === true){
+        //set winner modal
+        setTimeout(announceWinner,2000);
+        return;
+    }
     setTimeout(resetFirstandSecondCardVar,2000); // gives a sec before announcing player turn
 
 }
 
-function currentPlayerPositionNotification(){
-    screenClickable = !screenClickable;// allows player to click again afterwards.
+function announceWinner() {
+    // check who died.
+    //show modal.
+    //time out modal and reset game.
+    if (player1.health <= 0 && player2.health <=0){
+        $('#winnerModalName').text('Its a tie!');
+    }else if(player1.health <= 0 ){
+        $('#winnerModalName').text(player2.name);
+    }else{
+        $('#winnerModalName').text(player1.name);
+    }
+    toggleModal('#winnerModal');
+    setTimeout(toggleModal,5000,'#winnerModal');
+    setTimeout(resetGame,5500);
+}
 
-    if (currentPlayer){
+function currentPlayerPositionNotification(){
+    screenClickable = true;// allows player to click again afterwards.
+
+    if (currentPlayerTurn){
+        targetPlayer = player2;
+        currentPlayer = player1;
         toggleModal('#modalPlayer1');
         setTimeout(toggleModal,1500,'#modalPlayer1');
     }else{
+        targetPlayer = player1;
+        currentPlayer = player2;
         toggleModal('#modalPlayer2');
         setTimeout(toggleModal,1500,'#modalPlayer2');
     }
+}
+
+function updatePlayerStats(){
+    console.log('updatePlayer stats ran!');
+    for (var i = 0; i < 2; i++){
+        totalPlayers[i].updateStatDisplay();
+    }
+}
+
+function resetPlayerAbilitys(){
+    for (var i = 0; i < 2; i++){
+        totalPlayers[i].resetPlayerStats();
+    }
+    //flip over player held cards
 }
 
 function toggleModal( _string ) {
@@ -217,16 +216,6 @@ function checkCardName(_inputImageSrc){
 }
 
 function activateCardAbility(_cardName) {
-
-    cardName[_cardName].ability();
-    // switch(cardName){
-//     case 'attack':
-//         //
-//         break;
-//     case 'bahamut':
-//         //damage everyone
-//         break;
-// }
-
+    cardName[_cardName].ability(); // simpliy calls card ability.
 }
 
